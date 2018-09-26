0e6639622e5a11c7596a3bab73395ca020651dc7
zhangliping <zhangliping@ifunq.com>
2018-09-26 11:37:26 +0800

    add all files

---
+++ .idea/fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,21 @@
+<template>
+    <view></view>
+</template>
+
+<script>
+  import wepy from 'wepy'
+
+  export default class Page extends wepy.page {
+    config = {
+      navigationBarTitleText: 'Page'
+    }
+    data = {}
+    method = {}
+    events = {}
+    onLoad() {};
+  }
+</script>
+
+<style scoped>
+
+</style>
